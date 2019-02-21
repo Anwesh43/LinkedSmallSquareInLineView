94678244e007c92bc483317ef29ad7a329f46224
Anwesh43 <anweshthecool0@gmail.com>
2019-02-21 09:04:59 +0530

    adding libraries and defining constants

--- smallsquarelineview/src/main/java/com/anwesh/uiprojects/smallsquarelineview/SmallSquareInLineView.kt
+++ smallsquarelineview/src/main/java/com/anwesh/uiprojects/smallsquarelineview/SmallSquareInLineView.kt
@@ -3,3 +3,21 @@ package com.anwesh.uiprojects.smallsquarelineview
 /**
  * Created by anweshmishra on 21/02/19.
  */
+
+import android.view.View
+import android.view.MotionEvent
+import android.app.Activity
+import android.content.Context
+import android.graphics.Paint
+import android.graphics.Canvas
+import android.graphics.Color
+
+val nodes : Int = 5
+val square : Int = 3
+val lines : Int = 2
+val scGap : Float = 0.05f
+val scDiv : Double = 0.51
+val strokeFactor : Int = 90
+val sizeFactor : Float = 2.9f
+val foreColor : Int = Color.parseColor("#1565C0")
+val backColor : Int = Color.parseColor("#212121")
