cdc9c296ed13ab6112978b188f289caff352811a
Anwesh43 <anweshthecool0@gmail.com>
2019-02-21 09:52:12 +0530

    adding SSLNode class

--- smallsquarelineview/src/main/java/com/anwesh/uiprojects/smallsquarelineview/SmallSquareInLineView.kt
+++ smallsquarelineview/src/main/java/com/anwesh/uiprojects/smallsquarelineview/SmallSquareInLineView.kt
@@ -123,4 +123,49 @@ class SmallSquareInLineView(ctx : Context) : View(ctx) {
             }
         }
     }
+
+    data class SSLNode(var i : Int, val state : State = State()) {
+
+        private var next : SSLNode? = null
+        private var prev : SSLNode? = null
+
+        init {
+            addNeighbor()
+        }
+
+        fun addNeighbor() {
+            if (i < nodes - 1) {
+                next = SSLNode(i + 1)
+                next?.prev = this
+            }
+
+        }
+
+        fun draw(canvas : Canvas, paint : Paint) {
+            canvas.drawSSLNode(i, state.scale, paint)
+            next?.draw(canvas, paint)
+        }
+
+        fun update(cb : (Int, Float) -> Unit) {
+            state.update {
+                cb(i, it)
+            }
+        }
+
+        fun startUpdating(cb : () -> Unit) {
+            state.startUpdating(cb)
+        }
+
+        fun getNext(dir : Int, cb : () -> Unit) : SSLNode {
+            var curr : SSLNode? = prev
+            if (dir == 1) {
+                curr = next
+            }
+            if (curr != null) {
+                return curr
+            }
+            cb()
+            return this 
+        }
+    }
 }
